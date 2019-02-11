50163faf4df128d969079c9060bd3b9e74e29bbb
Ilya Tskhovrebov <ilyatskhovrebov@gmail.com>
2019-02-11 13:23:14 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Ilya Tskhovrebov on 11/02/2019.
+//  Copyright © 2019 Miyamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
